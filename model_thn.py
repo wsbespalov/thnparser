60120a0685d74fd3b30aac7e585ab0f0e8c78eee
wsbespalov <ws.bespalov@gmail.com>
2018-07-05 14:08:11 +0500

    complete

--- model_thn.py
+++ model_thn.py
@@ -2,6 +2,7 @@ import os
 import peewee
 from datetime import datetime
 
+from playhouse.postgres_ext import ArrayField
 from settings import SETTINGS
 
 POSTGRES = SETTINGS.get("postgres", {})
@@ -31,19 +32,58 @@ database = peewee.PostgresqlDatabase(
 class THN(peewee.Model):
     class Meta:
         database = database
-        # ordering = (, )
+        ordering = ("thn_id", )
         table_name = "vulnerabilities_thn"
 
     id = peewee.PrimaryKeyField(null=False)
+    index = peewee.TextField(default="")
+    thn_id = peewee.TextField(default="")
+    score = peewee.TextField(default="")
+    sort = peewee.TextField(default="")
+    lastseen = peewee.TextField(default="")
+    object_type = peewee.TextField(default="")
+    object_types = ArrayField(peewee.TextField, default=[])
+    description = peewee.TextField(default="")
+    published = peewee.TextField(default="")
+    reporter = peewee.TextField(default="")
+    type = peewee.TextField(default="")
+    title = peewee.TextField(default="")
+    enchantments_score_vector = peewee.TextField(default="")
+    enchantments_score_value = peewee.TextField(default="")
+    bulletin_family = peewee.TextField(default="")
+    cvelist = ArrayField(peewee.TextField, default=[])
+    modified = peewee.TextField(default="")
+    href = peewee.TextField(default="")
+    cvss_score = peewee.TextField(default="")
+    cvss_vector = peewee.TextField(default="")
 
     def __unicode__(self):
         return "thn"
 
-    # def __str__(self):
-    #     return str(self.cve_id)
+    def __str__(self):
+        return str(self.thn_id)
 
     @property
     def to_json(self):
         return dict(
-
+            id=self.id,
+            index=self.index,
+            thn_id=self.thn_id,
+            score=self.score,
+            lastseen=self.lastseen,
+            object_type=self.object_type,
+            object_types=self.object_types,
+            description=self.description,
+            published=self.published,
+            reporter=self.reporter,
+            type=self.type,
+            title=self.title,
+            enchantments_score_vector=self.enchantments_score_vector,
+            enchantments_score_value=self.enchantments_score_value,
+            bulletin_family=self.bulletin_family,
+            cvelist=self.cvelist,
+            modified=self.modified,
+            href=self.href,
+            cvss_score=self.cvss_score,
+            cvss_vector=self.cvss_vector
         )
--- thnparser.py
+++ thnparser.py
@@ -162,17 +162,84 @@ def check_if_thn_item_in_postgres(item_in_json):
         return False, sid
     return False, thns[0].to_json["thn_id"]
 
-def create_thn_item_in_postgres():
-    pass
+def create_thn_item_in_postgres(item_in_json):
+    connect_database()
+    thn = THN(
+        index=item_in_json["index"],
+        thn_id=item_in_json["thn_id"],
+        score=item_in_json["score"],
+        sort=item_in_json["sort"],
+        lastseen=item_in_json["lastseen"],
+        object_type=item_in_json["object_type"],
+        object_types=item_in_json["object_types"],
+        description=item_in_json["description"],
+        published=item_in_json["published"],
+        reporter=item_in_json["reporter"],
+        type=item_in_json["type"],
+        title=item_in_json["title"],
+        enchantments_score_vector=item_in_json["enchantments_score_vector"],
+        enchantments_score_value=item_in_json["enchantments_score_value"],
+        bulletin_family=item_in_json["bulletin_family"],
+        cvelist=item_in_json["cvelist"],
+        modified=item_in_json["modified"],
+        href=item_in_json["href"],
+        cvss_score=item_in_json["cvss_score"],
+        cvss_vector=item_in_json["cvss_vector"]
+    )
+    thn.save()
+    disconnect_database()
+    return thn.id
 
+def update_thn_item_in_postgres(item_in_json, sid):
+    connect_database()
+    modified = False
 
-def create_thn_item_in_postgres():
-    pass
+    thn = THN.get_by_id(sid)
 
+    if thn.score != item_in_json["score"] or \
+        thn.sort != item_in_json["sort"] or \
+        thn.object_type != item_in_json["object_type"] or \
+        thn.description != item_in_json["description"] or \
+        thn.reporter != item_in_json["reporter"] or \
+        thn.type != item_in_json["type"] or \
+        thn.title != item_in_json["title"] or \
+        thn.enchantments_score_vector != item_in_json["enchantments_score_vector"] or \
+        thn.enchantments_score_value != item_in_json["enchantments_score_value"] or \
+        thn.bulletin_family != item_in_json["bulletin_family"] or \
+        thn.cvelist != item_in_json["cvelist"] or \
+        thn.href != item_in_json["href"] or \
+        thn.cvss_score != item_in_json["cvss_score"] or \
+            thn.cvss_vector != item_in_json["cvss_vector"]:
+        modified = True
 
-def update_thn_item_in_postgres():
-    pass
-
+    if modified:
+        item_in_json["published"] = datetime.utcnow() if item_in_json["published"] == "undefined" else item_in_json["published"]
+        item_in_json["lastseen"] = datetime.utcnow() if item_in_json["lastseen"] == "undefined" else item_in_json["lastseen"]
+        thn.index = item_in_json["index"]
+        thn.thn_id = item_in_json["thn_id"]
+        thn.score = item_in_json["score"]
+        thn.sort = item_in_json["sort"]
+        thn.lastseen = item_in_json["lastseen"]
+        thn.object_type = item_in_json["object_type"]
+        thn.object_types = item_in_json["object_types"]
+        thn.description = item_in_json["description"]
+        thn.published = item_in_json["published"]
+        thn.reporter = item_in_json["reporter"]
+        thn.type = item_in_json["type"]
+        thn.title = item_in_json["title"]
+        thn.enchantments_score_vector = item_in_json["enchantments_score_vector"]
+        thn.enchantments_score_value = item_in_json["enchantments_score_value"]
+        thn.bulletin_family = item_in_json["bulletin_family"]
+        thn.cvelist = item_in_json["cvelist"]
+        thn.modified = item_in_json["modified"]
+        thn.href = item_in_json["href"]
+        thn.cvss_score = item_in_json["cvss_score"]
+        thn.cvss_vector = item_in_json["cvss_vector"]
+        thn.save()
+    disconnect_database()
+    if modified:
+        return "modified"
+    return "skipped"
 
 def create_or_update_thn_item_in_postgres(item_in_json):
     exists, sid = check_if_thn_item_in_postgres(item_in_json)
@@ -194,7 +261,7 @@ def update_thn_vulners():
             for item_in_json in data_in_json:
                 thn = {}
                 thn["index"] = item_in_json.get("_index", "thn")
-                thn["thn_id"] = item_in_json.het("_id", undefined)
+                thn["thn_id"] = item_in_json.get("_id", undefined)
                 thn["score"] = item_in_json.get("_score", None)
                 if thn["score"] is None:
                     thn["score"] = undefined
@@ -203,7 +270,9 @@ def update_thn_vulners():
                 source = item_in_json.get("_source", {})
                 thn["lastseen"] = source.get("lastseen", datetime.utcnow())
                 thn["object_type"] = source.get("_object_type", undefined)
-                thn["object_types"] = source.get("_object_type", [])
+                thn["object_types"] = []
+                for _ in source.get("_object_types", []):
+                    thn["object_types"].append(_)
                 thn["references"] = source.get("references", [])
                 thn["description"] = source.get("description", "")
                 thn["published"] = source.get("published", datetime.utcnow())
@@ -216,7 +285,9 @@ def update_thn_vulners():
                 thn["enchantments_score_vector"] = score.get("vector", "NONE")
                 thn["enchantments_score_value"] = str(score.get("value", "0.0"))
                 thn["bulletin_family"] = source.get("bulletinFamily", undefined)
-                thn["cvelist"] = source.get("cvelist", [])
+                thn["cvelist"] = []
+                for _ in source.get("cvelist", []):
+                    thn["cvelist"].append(_)
                 thn["object_type"] = source.get("_object_type", undefined)
                 thn["modified"] = source.get("modified", datetime.utcnow())
                 thn["href"] = source.get("href", undefined)
